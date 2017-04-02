4fa078b2a4e990d473d958cce76ed5201e8b4e6e
Fan Jin <fanjin@nbnco.com.au>
2017-04-02 22:59:38 +1000

    init

---
+++ KidsCalculator/StructsEnum.swift
@@ -0,0 +1,14 @@
+//
+//  StructsEnum.swift
+//  KidsCalculator
+//
+//  Created by Fan Jin on 2/4/17.
+//  Copyright © 2017 Fan Jin. All rights reserved.
+//
+
+import Foundation
+
+enum CalculatorState: Int {
+    case NEW_START = 0
+    case ENTERING = 1
+}
--- KidsCalculator/ViewController.swift
+++ KidsCalculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    var currentStatus:CalculatorState = CalculatorState.NEW_START
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,62 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func convertNumberToOperator(value:Int) ->String {
+        var symbol:String?
+        if(value == 10) {
+            symbol = "."
+        }
+        else if(value == 11){
+            symbol = "+"
+        }
+        else if(value == 12){
+            symbol = "-"
+        }
+        else if(value == 13){
+            symbol = "*"
+        }
+        else if(value == 14){
+            symbol = "/"
+        }
+        else if(value == 17){
+            symbol = "("
+        }
+        else if(value == 18){
+            symbol = ")"
+        }
+        else {
+            symbol = String(value)
+        }
+        
+        return symbol!
+        
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton){
+        if(currentStatus == CalculatorState.ENTERING){
+            resultLabel.text = resultLabel.text! + convertNumberToOperator(value: sender.tag)
+        }
+        else{
+            resultLabel.text = convertNumberToOperator(value: sender.tag)
+        }
+        currentStatus = CalculatorState.ENTERING
+    }
+    
+    @IBAction func equalsClicked(_ sender: UIButton){
+        currentStatus = CalculatorState.NEW_START
+        
+        
+        resultLabel.text = String(Double("\( resultLabel.text! )")!)
+        
+    }
+    
+    @IBAction func clearClicked(_ sender: UIButton){
+        resultLabel.text = "0"
+        currentStatus = CalculatorState.NEW_START
+    }
+    
+    
 
 
 }
